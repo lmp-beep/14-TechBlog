aa5f1290943ced3d1b961fdb75742a3aaaf47966
Lisa Pessin <lisapessin@gmail.com>
2021-06-28 09:50:12 -0400

    trying different comment functions

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,45 +2,67 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
-    });
-    res.json(newComment);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.post('/', withAuth, async (req, res) => {
+//   try {
+//     const newComment = await Comment.create({
+//       ...req.body,
+//       userId: req.session.userId,
+//     });
+//     res.json(newComment);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // router.get('/comment/:id', (req, res) => {
 //     res.render('post');
 //   });
 
-module.exports = router;
+
 
 
 
   
 
 
-// router.post("/", withAuth, async (req, res) => {
-//   try {
-//     const newComment = await Comment.create(req.body, {
-//       userId: req.session.userId,
-//     });
-//     res.json(newComment.body);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create(req.body, {
+      userId: req.session.userId,
+    });
+    res.json(newComment.body);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get("/", withAuth, async (req, res) => {
-//   try {
-//     const allComments = await Comment.findAll({});
-//     res.json(allComments);
-//   } catch (err) {}
-// });
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const allComments = await Comment.findAll({});
+    res.json(allComments);
+  } catch (err) {}
+});
 
-// module.exports = router;
+
+
+
+
+
+
+// router.post("/", async (req, res) => {
+//     console.log(req.session)
+//     try {
+//       const addComment = await Comment.create({
+//         // user_id: req.session.user_id,
+//         content: req.body.addComment
+//       });
+//       res.status(200).json(addComment);
+//       console.log(addComment + " router post")
+//     } catch (err) {
+//       res.status(400).json(err);
+      
+//     }
+//   });
+
+
+module.exports = router;
